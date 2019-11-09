bcd1d5b3912074fee8a2a5ba3f43cb99a4a06da1
harrychen1995 <harrychen1995@yahoo.com>
2019-11-08 20:13:48 -0500

    ok

--- generate_test_vector.py
+++ generate_test_vector.py
@@ -7,15 +7,70 @@ class Test_Generator:
         self.num_per_case = num_per_case
 
     
-    def convert_vector_to_number(self):
-        pass 
 
-    def convert_number_to_vector(self):
-        pass 
 
+    def binary_to_real_number(self, bin_string):
+        
+        if  '.' in bin_string:
+
+            integer , decimal = bin_string.split('.')
+
+            sum = 0.0
+            for n, i in enumerate(integer[::-1]):
+                if i == '1':
+                    sum += 2**n
+            
+            for n, i in enumerate(decimal):
+                if i == '1':
+                    sum += (1/2)**(n+1)
+        else:
+            sum = 0
+            for n, i in enumerate(bin_string[::-1]):
+                if i == '1':
+                    sum += 2**n
+        return sum
+            
+         
+
+
+
+
+    def convert_vector_to_normalized_num(self, vector):
+        
+        Sig = -1 if vector[0] else 1
+        Exp = ''.join(list(map( lambda x : str(x), vector[1:9])))
+        Fract = '1.'+''.join(list(map( lambda x : str(x), vector[9:])))
+
+    
+        Exp = self.binary_to_real_number(Exp)
+        Fract = self.binary_to_real_number(Fract)
+        sum = Sig*2**(Exp-127)*Fract
+        
+        return sum
+
+
+    def convert_normalized_numb_to_vector(self, vector):
+        pass 
+    
+    def convert_vector_to_denormalized_num(self, vector):
+
+        Sig = -1 if vector[0] else 1
+        Fract = '0.'+''.join(list(map( lambda x : str(x), vector[9:])))
+        
+        Fract = self.binary_to_real_number(Fract)
+
+        sum = Sig*2**(-126)*Fract
+        
+        return sum
+
+    def convert_denormalized_numb_to_vector(self, vector):
+        pass 
 
 
 
 if __name__ == "__main__":
-    pass
+    
+    t = Test_Generator()
+    r = [0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    print(t.convert_vector_to_denormalized_num(r))
 
