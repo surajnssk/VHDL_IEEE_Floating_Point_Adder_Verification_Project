c4828e950cda57ef7c5423dcff6749712cf87e43
harrychen1995 <harrychen1995@yahoo.com>
2019-11-08 19:12:03 -0500

    class added

--- generate_test_vector.py
+++ generate_test_vector.py
@@ -1,2 +1,20 @@
 import random 
 import os
+
+
+class Generator:
+    def __init__(self):
+        pass
+
+    
+    def convert_vect_to_number(self):
+        pass 
+
+    def convert_number_to_vect(self):
+        pass 
+
+
+
+
+if __name__ = "__main__":
+
